c4c8211835573800f02ca12db187a77e6ed5d6d2
aliyousafzai183 <warisallah923@gmail.com>
2023-04-28 10:50:11 +0500

    COunterchecked and added db for onboarding

--- src/routes/RootNavigator.js
+++ src/routes/RootNavigator.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RouteName from './RouteName';
 import colors from '../themes/colors';
+
 // screens
 import EmployeeData from '../screens/UserData/EmployeeData';
 import ExpensesDataScreen from '../screens/UserData/ExpensesDataScreen';
@@ -12,11 +13,75 @@ import RevenueDataScreen from '../screens/UserData/RevenueDataScreen';
 import YourNameScreen from '../screens/UserData/YourNameScreen';
 import SplashScreen from '../screens/SplashScreen';
 import TipDetailScreen from '../screens/TipDetailScreen';
+import SecondSplashScreen from '../screens/SecondSplashScreen';
+
+// storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import SideNavigator from './SideNavigator';
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
+
+    const [showOnboarding, setShowOnboarding] = useState(false);
+
+    useEffect(() => {
+        async function checkOnboardingStatus() {
+            try {
+                const value = await AsyncStorage.getItem('@app:onboarding');
+                setShowOnboarding(value === null);
+            } catch (error) {
+                // Handle error
+            }
+        }
+        checkOnboardingStatus();
+    }, []);
+
+    const handleOnboardingComplete = async () => {
+        try {
+            await AsyncStorage.setItem('@app:onboarding', 'true');
+            setShowOnboarding(false);
+        } catch (error) {
+            // Handle error
+        }
+    };
+
+    if (showOnboarding) {
+        return (
+            <NavigationContainer >
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false,
+                        headerStyle: {
+                            backgroundColor: colors.top
+                        },
+                        headerTintColor: colors.text
+                    }}
+                >
+
+                    <Stack.Screen name={RouteName.SPLASH_SCREEN} component={SplashScreen} />
+                    <Stack.Screen name={RouteName.YOUR_NAME_SCREEN} component={YourNameScreen} />
+                    <Stack.Screen name={RouteName.ORGANIZATION_NAME_SCREEN} component={OrganizationNameScreen} />
+                    <Stack.Screen name={RouteName.EMPLOYEE_DATA_SCREEN} component={EmployeeData} />
+                    <Stack.Screen name={RouteName.REVENUE_DATA_SCREEN} component={RevenueDataScreen} />
+                    <Stack.Screen name={RouteName.EXPENSE_DATA_SCREEN} component={ExpensesDataScreen} />
+                    <Stack.Screen name={RouteName.RESULT_SCREEN} component={ResultScreen} />
+                    <Stack.Screen name={RouteName.MAIN_HOME_SCREEN} component={SideNavigator} />
+                    <Stack.Screen
+                        name={RouteName.TIP_DETAIL_SCREEN}
+                        component={TipDetailScreen}
+                        options={{
+                            headerShown: true,
+                            title: 'Tip Description',
+                        }}
+                    />
+
+                </Stack.Navigator>
+            </NavigationContainer >
+        );
+
+    }
+
     return (
         <NavigationContainer >
             <Stack.Navigator
@@ -29,13 +94,13 @@ const RootNavigator = () => {
                 }}
             >
 
-                <Stack.Screen name={RouteName.SPLASH_SCREEN} component={SplashScreen} />
-                <Stack.Screen name={RouteName.YOUR_NAME_SCREEN} component={YourNameScreen} />
+                <Stack.Screen name={RouteName.SPLASH_SCREEN} component={SecondSplashScreen} />
+                {/* <Stack.Screen name={RouteName.YOUR_NAME_SCREEN} component={YourNameScreen} />
                 <Stack.Screen name={RouteName.ORGANIZATION_NAME_SCREEN} component={OrganizationNameScreen} />
                 <Stack.Screen name={RouteName.EMPLOYEE_DATA_SCREEN} component={EmployeeData} />
                 <Stack.Screen name={RouteName.REVENUE_DATA_SCREEN} component={RevenueDataScreen} />
                 <Stack.Screen name={RouteName.EXPENSE_DATA_SCREEN} component={ExpensesDataScreen} />
-                <Stack.Screen name={RouteName.RESULT_SCREEN} component={ResultScreen} />
+                <Stack.Screen name={RouteName.RESULT_SCREEN} component={ResultScreen} /> */}
                 <Stack.Screen name={RouteName.MAIN_HOME_SCREEN} component={SideNavigator} />
                 <Stack.Screen
                     name={RouteName.TIP_DETAIL_SCREEN}
@@ -49,5 +114,7 @@ const RootNavigator = () => {
             </Stack.Navigator>
         </NavigationContainer >
     );
+
+
 }
 export default RootNavigator;
---
+++ src/screens/SecondSplashScreen.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import RouteName from '../routes/RouteName';
+import colors from '../themes/colors';
+import LinearGradient from 'react-native-linear-gradient';
+
+const SecondSplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace(RouteName.MAIN_HOME_SCREEN);
+    }, 2100);
+  }, []);
+
+  return (
+    <LinearGradient colors={[colors.bottom, colors.center, colors.bottom]} style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../images/splashLogo.png')} style={styles.logo} />
+      </View>
+      <Text style={styles.text}>Business Growth</Text>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  text: {
+    marginBottom: 50,
+    fontSize: 24,
+    color: colors.text,
+  },
+});
+
+export default SecondSplashScreen;
